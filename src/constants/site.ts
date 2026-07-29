070b32f8d3f7254d3140e174784894608a173739
shivam562-j <krshivam998@gmail.com>
2026-07-29 21:07:52 +0530

    site conf changes

--- src/constants/site.ts
+++ src/constants/site.ts
@@ -4,7 +4,7 @@ export const SITE_CONFIG = {
   description:
     "Buy, sell, and rent properties and products in Khurja. Your trusted local marketplace.",
   url: process.env.NEXT_PUBLIC_SITE_URL || "https://khurjadeals.com",
-  phone: "+91 7906896546",
+  phone: "+91 9258550570",
   whatsapp: "917906896546",
   email: "info@khurjadeals.com",
   address: "Khurja, Bulandshahr, Uttar Pradesh, India",
@@ -12,9 +12,9 @@ export const SITE_CONFIG = {
   whiteLogo: "/logos/white-logo.webp",
   favicon: "/favicon.ico",
   social: {
-    instagram: "",
-    facebook: "",
+    instagram: "https://www.instagram.com/khurjadeals_",
+    facebook: "https://www.facebook.com/share/1EkNBfPMHr/",
     twitter: "",
-    youtube: "",
+    youtube: "https://www.youtube.com/@khurjadeal",
   },
 } as const;
